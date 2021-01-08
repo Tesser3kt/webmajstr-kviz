51a3ce017cb55c433d28d1d82085635b3346e8f2
Aleks Golotvinov <golotvinov@gmail.com>
2021-01-08 13:57:41 +0100

    Uprava stylu (sjednoceni s hlavni strankou) + optimalizace JS

--- js/script.js
+++ js/script.js
@@ -100,8 +100,8 @@ function showQuetions(index){
     }
 }
 
-let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
-let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+let tickIconTag = '<div class="icon tick"></div>';
+let crossIconTag = '<div class="icon cross"></div>';
 
 function optionSelected(answer){
     clearInterval(counter);
